aebb9e8d2379e3007099d5b4da86abeb696e8cad
fajrifarid <fajrinfajrin55@gmail.com>
2024-02-23 21:45:05 +0800

    feat: registter when the form is not complete, can't click it and when it completed it will direct to login page and back botton to first page

--- src/components/auth/register/register.jsx
+++ src/components/auth/register/register.jsx
@@ -1,16 +1,19 @@
 "use client";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export const Register = () => {
+  const router = useRouter();
+  const [formValues, setFormValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
   async function handleSubmitRegister(event) {
-    //tidak me-refresh
     event.preventDefault();
 
-    //how to get the value
-    const name = event.target.name.value;
-    const email = event.target.email.value;
-    const password = event.target.password.value;
-
-    // console.log({ name, email, password });
+    const { name, email, password } = formValues; // Mengambil nilai dari formValues yang telah diperbarui
 
     //fetch the api
     const res = await fetch("https://eventmakers-api.fly.dev/auth/register", {
@@ -26,16 +29,59 @@ export const Register = () => {
     });
     const data = await res.json();
     console.log(data);
+
+    router.push("/login");
   }
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+
+  // disable button jika ada field yang kosong
+  const isFormValid =
+    formValues.name && formValues.email && formValues.password;
 
   return (
     <div>
-      <h2>Register</h2>
+      <h2 className="mb-2 font-bold text-xl text-center">Register</h2>
       <form onSubmit={handleSubmitRegister}>
-        <input name="name" placeholder="name" />
-        <input name="email" placeholder="email" />
-        <input name="password" placeholder="password" />
-        <button>register</button>
+        <h2>User Name</h2>
+        <input
+          name="name"
+          placeholder="name"
+          value={formValues.name}
+          type="text"
+          className="border-2 border-black p-4 mb-4 block rounded-lg w-full"
+          onChange={handleChange}
+        />
+        <h2>Email</h2>
+        <input
+          name="email"
+          placeholder="email"
+          value={formValues.email}
+          type="email"
+          className="border-2 border-black p-4 mb-4 block rounded-lg w-full"
+          onChange={handleChange}
+        />
+        <h2>Password</h2>
+        <input
+          name="password"
+          placeholder="password"
+          value={formValues.password}
+          type="password"
+          className="border-2 border-black p-4 mb-4 block rounded-lg w-full"
+          onChange={handleChange}
+        />
+        <button
+          className="bg-indigo-600 text-white p-2 rounded-xl w-full"
+          disabled={!isFormValid}
+        >
+          register
+        </button>
       </form>
     </div>
   );
