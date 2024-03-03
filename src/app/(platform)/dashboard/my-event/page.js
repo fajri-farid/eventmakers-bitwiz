bc03da372ef9caace3efcc3d44534e896d250275
fajri-farid <fajrijry14@gmail.com>
2024-03-03 22:19:48 +0800

    feat: show the event that i made in myevent

--- src/app/(platform)/dashboard/my-event/page.js
+++ src/app/(platform)/dashboard/my-event/page.js
@@ -4,33 +4,43 @@ import ShowEvents from "@/components/mainpage/showEvent";
 import { DashHeaderAfter } from "@/components/sharedUI/afterlogin/DashHeaderAfter";
 import { Footer } from "@/components/sharedUI/footer/Footer";
 import ShowEventsForMyEvent from "@/components/myEvent/showEventForMyEvent";
+import { getMyEvents } from "@/components/myEvent/onlymine";
+import { session } from "@/components/myEvent/session";
 
-async function getUsers() {
-  const token = cookies().get("token").value;
-  // return token;
-  const res = await fetch("https://eventmakers-api.fly.dev/users/", {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-  const data = await res.json();
-  return data;
-}
-
-export async function getEvents() {
-  const res = await fetch("https://eventmakers-api.fly.dev/events/");
-  const data = await res.json();
-  return data;
+async function getEvents() {
+  try {
+    const res = await fetch("https://eventmakers-api.fly.dev/events", {
+      method: "GET",
+      cache: "no-cache",
+    });
+    if (!res.ok) {
+      throw new Error("Failed to get event data.");
+    }
+    const { data } = await res.json();
+    return data.reverse();
+  } catch (error) {
+    throw new Error(error.message);
+  }
 }
 
 export default async function Page() {
-  const usersData = await getUsers();
-  const { data } = await getEvents();
+  // const { createdEvents } = await getMyEvents(usersData.id);
   // console.log(usersData);
+  const { userData } = session();
+  const authorID = userData.id;
+  let events;
+
+  try {
+    const data = await getEvents();
+    const userEvent = data?.filter((item) => item.events.author === authorID);
+    events = userEvent;
+  } catch (error) {
+    console.error("Error fetching event list:", error);
+  }
   return (
     <div>
       <DashHeaderAfter />
-      <ShowEventsForMyEvent data={data} />
+      <ShowEventsForMyEvent data={events} userData={userData} />
       <div className="mt-20">
         <Footer />
       </div>
---
+++ src/components/myEvent/onlymine.jsx
@@ -0,0 +1,35 @@
+export async function getMyEvents(userId) {
+  const [dataEvent, setDataEvent] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [user, setUser] = useState("");
+
+  useEffect(() => {
+    const localUser = JSON.parse(localStorage.getItem("user"));
+    setUser(localUser);
+  }, []);
+
+  async function getCreatedUserEvents() {
+    const res = await fetch("https://eventmakers-api.fly.dev/events/", {
+      cache: "no-store",
+    });
+    const { data } = await res.json();
+
+    let createdEvents = Array.from(data).filter((e) => {
+      return e.events.author === user.id;
+    });
+
+    return createdEvents;
+  }
+
+  useEffect(() => {
+    const handleDatas = async () => {
+      const datas = await getCreatedUserEvents();
+      datas.length !== 0 ? setDataEvent(datas) : setDataEvent(null);
+      setIsLoading(false);
+    };
+
+    handleDatas();
+  });
+
+  return { dataEvent, isLoading };
+}
